11019b9904ed3b9aeddc1e53303c693d3a191054
杜帅(dushuai) <dushuai@qdingnet.cn>
2020-10-16 10:55:11 +0800

    remove

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,40 +1,15 @@
-import { TreeNode, useTree, useTreeNode } from "@/lib/cdk/Tree";
-import { defineComponent, onMounted, ref, watch } from "vue";
-
-interface TestNode extends TreeNode {
-  fuck: string;
-}
+import { defineComponent } from "vue";
 
 const testChildren = defineComponent({
   name: "test",
-  props: {
-    index: { type: Number, default: 0 },
-  },
   setup(props) {
-    const treeNode = useTreeNode("test", props.index);
-    watch(treeNode.currentList, console.log, { immediate: true });
-    return () => <div>this is test {treeNode.currentNode.value?.fuck}</div>;
+    return () => <div>this is test</div>;
   },
 });
 
 export default defineComponent({
   name: "home",
   setup() {
-    const tree = useTree<TestNode>("test", [
-      { children: [{ fuck: "234" }], fuck: "shit" },
-    ]);
-    onMounted(() => {
-      setTimeout(() => {
-        tree.value[0].fuck = "fuck you";
-        tree.value.push({ fuck: "shame on you" });
-      }, 1000);
-    });
-    return () => (
-      <div>
-        {tree.value.map((_, index) => (
-          <testChildren key={index} index={index} />
-        ))}
-      </div>
-    );
+    return () => <div></div>;
   },
 });
