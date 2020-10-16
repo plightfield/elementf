10bb08dd58b5bfb4d81157b4ea1fbf77dbc540fd
杜帅(dushuai) <dushuai@qdingnet.cn>
2020-10-16 17:08:42 +0800

    change

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, renderSlot } from "vue";
+import { PromiseHandler } from "@/lib/cdk";
 const testChildren = defineComponent({
   name: "test",
   setup(props, ctx) {
@@ -6,9 +7,26 @@ const testChildren = defineComponent({
   },
 });
 
+const wait = () =>
+  new Promise((res) => {
+    setTimeout(() => {
+      res(new Date());
+    }, 2000);
+  });
+
 export default defineComponent({
   name: "home",
   setup() {
-    return () => <div></div>;
+    const { data, loading, error } = PromiseHandler(wait, {
+      debounce: 300,
+      loop: true,
+    });
+    return () => (
+      <div>
+        {data}
+        {loading}
+        {error}
+      </div>
+    );
   },
 });
